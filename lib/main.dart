a6e4310b7d3f1cbf23bbdc49f5a845147b098ace
Erwan <erwan.merly@etu.imt-lille-douai.fr>
2021-02-01 18:11:33 +0100

    Ajout d'une barre de navigation Intégration du modèle des oeuvres

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Mediatheque'),
     );
   }
 }
@@ -50,47 +40,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -107,7 +68,77 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          iconSize: 20.0,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              title: Text('Movies'),
+              icon: Icon(Icons.local_movies),
+            ),
+            BottomNavigationBarItem(
+              title: Text('Books'),
+              icon: Icon(Icons.book_outlined),
+            ),
+            BottomNavigationBarItem(
+              title: Text('Series'),
+              icon: Icon(Icons.live_tv),
+            ),
+            BottomNavigationBarItem(
+              title: Text('Comics'),
+              icon: Icon(Icons.filter_outlined),
+            ),
+            BottomNavigationBarItem(
+              title: Text('Albums'),
+              icon: Icon(Icons.my_library_music),
+            ),
+          ],
+      ),
     );
   }
 }
+
+class MediaModel {
+  String imageUrl;
+  String title;
+  String description;
+
+  // Constructor
+  MediaModel({this.imageUrl, this.title, this.description});
+}
+
+final series = [
+  MediaModel(
+    imageUrl:
+    'https://m.media-amazon.com/images/M/MV5BNzRlNGUzMmEtYTg0Ni00N2U2LTg4YWEtNDdlNmMwYjBlZDQ0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UY864_.jpg',
+    title: 'Lupin',
+    description: "Série Netflix ...",
+  ),
+  MediaModel(
+    imageUrl:
+    'https://upload.wikimedia.org/wikipedia/fr/6/67/StrangerThingslogo.png',
+    title: 'Stranger Things',
+    description: "Série Netflix ...",
+  ),
+  MediaModel(
+    imageUrl:
+    'https://m.media-amazon.com/images/M/MV5BNGEyOGJiNWEtMTgwMi00ODU4LTlkMjItZWI4NjFmMzgxZGY2XkEyXkFqcGdeQXVyNjcyNjcyMzQ@._V1_FMjpg_UX675_.jpg',
+    title: 'The Boys',
+    description: "Série Amazon ...",
+  ),
+];
+
+final bds = [
+  MediaModel(
+    imageUrl: 'images/bds/aldebaran.jpg',
+    title: 'Aldébaran',
+    description: "Les mondes d'Aldébaran ...",
+  ),
+  MediaModel(
+    imageUrl: 'images/bds/le_tueur.jpg',
+    title: 'Le tueur',
+    description: "Bd cynique sur le monde...",
+  ),
+];
